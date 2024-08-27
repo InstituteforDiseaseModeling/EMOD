7e994f9b1100ed40844c0caf5dd28427a9f03f6b
Daniel Bridenbecker <dbridenbecker@idmod.org>
2024-08-27 09:37:46 -0700

    Removed TargetGroupType which is an enum for TB (#32)

--- campaign/InterventionEnums.h
+++ campaign/InterventionEnums.h
@@ -15,17 +15,6 @@ namespace Kernel
         ENUM_VALUE_SPEC(ExplicitDiseaseState        , 6)
         ENUM_VALUE_SPEC(Pregnant                    , 7))
 
-    ENUM_DEFINE(TargetGroupType,
-        ENUM_VALUE_SPEC(Everyone                , 1) 
-        ENUM_VALUE_SPEC(Infected                , 2) 
-        ENUM_VALUE_SPEC(ActiveInfection         , 3) 
-        ENUM_VALUE_SPEC(LatentInfection         , 4) 
-        ENUM_VALUE_SPEC(MDR                     , 5)
-        ENUM_VALUE_SPEC(TreatmentNaive          , 6)
-        ENUM_VALUE_SPEC(HasFailedTreatment      , 7)
-        ENUM_VALUE_SPEC(HIVNegative             , 8)
-        ENUM_VALUE_SPEC(ActiveHadTreatment      , 9))
-        
     ENUM_DEFINE(TargetGender,
         ENUM_VALUE_SPEC(All     , 0)
         ENUM_VALUE_SPEC(Male    , 1)
